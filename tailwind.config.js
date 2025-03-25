99c56d47d9c7325fda7f8305d3cb64be15ac21fd
pimanzi <p.imanzi@alustudent.com>
2025-03-25 11:33:47 +0200

    chore: fixed the hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ export default {
   content: ['./src/**/*.{html,js}', './*.html'],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        40: 'repeat(40, minmax(0, 1fr))',
+      },
+
+      gridTemplateRows: {
+        40: 'repeat(40, minmax(0, 1fr))',
+      },
+      rotate: {
+        6: '6deg',
+        12: '12deg',
+        '-12': '-12deg',
+      },
       backgroundImage: {
         headerVector: "url('/images/vector5.png')",
       },
