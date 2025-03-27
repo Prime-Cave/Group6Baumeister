c7cb6f0126214a8921be928fb3c66783d645092b
pimanzi <p.imanzi@alustudent.com>
2025-03-27 11:15:31 +0200

    chore: fixed the hero section and the header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,37 @@ export default {
   content: ['./src/**/*.{html,js}', './*.html'],
   theme: {
     extend: {
+      width: {
+        quarter: '85%',
+        'nine-tenths': '90%',
+      },
+      gridColumnStart: {
+        16: '16',
+        23: '23',
+        26: '26',
+        28: '28',
+      },
+      gridColumnEnd: {
+        20: '20',
+        23: '23',
+        31: '31',
+        36: '36',
+        39: '39',
+        50: '50',
+      },
+      gridRowStart: {
+        18: '18',
+      },
+      gridRowEnd: {
+        15: '15',
+        16: '16',
+        27: '27',
+        28: '28',
+        31: '31',
+        35: '35',
+        40: '40',
+      },
+
       gridTemplateColumns: {
         40: 'repeat(40, minmax(0, 1fr))',
       },
@@ -10,17 +41,14 @@ export default {
       gridTemplateRows: {
         40: 'repeat(40, minmax(0, 1fr))',
       },
-      rotate: {
-        6: '6deg',
-        12: '12deg',
-        '-12': '-12deg',
-      },
+
       backgroundImage: {
-        headerVector: "url('/images/vector5.png')",
+        'header-Vector': "url('/images/vector5.png')",
       },
+
       colors: {
         primary: '#00672E',
-        mainBg: '#efecea',
+        secondary: '#efecea',
       },
     },
   },
